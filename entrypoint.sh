137b8aaac10fa38cdf4caa5fb23f85062454b2fc
Ramon Navarro Bosch <ramon.nb@gmail.com>
2016-10-22 18:15:30 -0400

    Initial docker image

---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+curl -X POST -H "Accept: application/json" -H "Authorization: Basic YWRtaW4=" -H "Content-Type: application/json" -d '{
+    "@type": "Plone Site",
+    "title": "Plone 1",
+    "id": "plone",
+    "description": "Description"
+}' "http://127.0.0.1:8080/zodb1/"
+
+
+curl -X POST -H "Accept: application/json" -H "Authorization: Basic YWRtaW4=" -H "Content-Type: application/json" -d '{
+    "prinrole": {
+        "Anonymous User": ["plone.Member", "plone.Reader"]
+    }
+}' "http://127.0.0.1:8080/zodb1/plone/@sharing"
+
+curl -X POST -H "Accept: application/json" -H "Authorization: Basic YWRtaW4=" -H "Content-Type: application/json" -d '{
+    "@type": "Item",
+    "title": "News",
+    "id": "news"
+}' "http://127.0.0.1:8080/plone/site1/"
+
+
+exec "$@"
